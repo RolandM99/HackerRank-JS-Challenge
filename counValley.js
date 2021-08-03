5104d6c2602f90c50d4066f66d698bb6bf88a3c1
rolandm99 <manfulmweze99@gmail.com>
2021-08-03 21:42:25 +0100

    Count Valleys

---
+++ counValley.js
@@ -0,0 +1,19 @@
+
+    //Counting Valleys
+    function countingValleys(n, s) {
+        let steps = s.split("");
+        let valleyCount = 0;
+        let currentSeaLevel = 0;
+        let valleyStatus = false;
+        steps.forEach(step => {
+          step === 'U' ? currentSeaLevel++ : currentSeaLevel--;
+            if(currentSeaLevel < 0 && !valleyStatus) {
+              valleyCount++;
+              valleyStatus = true
+            } else if(currentSeaLevel >= 0 && valleyStatus) {
+              valleyStatus= false
+            }
+          })
+          return valleyCount;
+        
+        }
